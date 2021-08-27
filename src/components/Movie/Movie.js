bfef8e338af4ab65e53e17b81db4d799b68bd6d7
Maria DelSignore <maria.ad27@gmail.com>
2021-08-27 17:20:06 -0600

    Add fetch data for specific movie
    Co-authored-by: Nina B <ninabrissey@users.noreply.github.com>

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,7 +1,41 @@
+import React, {Component} from 'react';
+import fetchData from '../../apiCalls';
 import './Movie.css';
 
-const Movie = () => {
+class Movie extends Component {
+  constructor() {
+    super()
+    this.state = {
+      movie: {}
+      video: ''
+    }
+    // console.log(this.state.movie)
+  }
+  
+  componentDidMount() {
+    // console.log('butts', this.props.movieID)
+    fetchData(`movies/${this.props.movieID}`).then((data) =>
+      this.setState({ movie: data.movie })
+    );
 
+    fetchData(`movies/${this.props.movieID}/videos`).then((data) =>
+    this.setState({ video: data })
+  );
+  }
+
+
+  render() {
+    const { title, poster_path, backdrop_path, release_date, overview, genres, budget, runtime, tagline, average_rating } = this.state.movie
+
+    return (
+      <section>
+        <h2>{title}</h2>
+        <h3>{average_rating}</h3>
+        <img src={poster_path} alt={`${title} movie poster`} />
+        <>
+      </section>
+    )
+  }  
 }
 
 
