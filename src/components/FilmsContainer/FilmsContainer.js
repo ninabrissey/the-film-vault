09cb128b8eb3a2389472f8519ff01097c8bb7bd6
Nina Brissey <nbrissey@gmail.com>
2021-08-26 20:46:50 -0600

    Adjusted the way data was accessed

--- src/components/FilmsContainer/FilmsContainer.js
+++ src/components/FilmsContainer/FilmsContainer.js
@@ -1,8 +1,8 @@
 import FilmCard from '../FilmCard/FilmCard';
 import './FilmsContainer.css';
 
-const FilmsContainer = (props) => {
-  const movieCards = props.movies.map((movie) => {
+const FilmsContainer = ({ movies }) => {
+  const movieCards = movies.map((movie) => {
     return <FilmCard key={movie.id} movie={movie} />;
   });
   return <section>{movieCards}</section>;
