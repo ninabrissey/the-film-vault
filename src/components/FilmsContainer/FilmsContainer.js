d76c7fe48b6ffa217925dd11cfa91882e895a7f3
Nina Brissey <nbrissey@gmail.com>
2021-08-28 13:29:01 -0600

    Pass showMovieDetails prop to FilmCard

--- src/components/FilmsContainer/FilmsContainer.js
+++ src/components/FilmsContainer/FilmsContainer.js
@@ -1,23 +1,22 @@
 import FilmCard from '../FilmCard/FilmCard';
 import './FilmsContainer.css';
 
-const FilmsContainer = ({ movies }) => {
+const FilmsContainer = ({ movies, showMovieDetails }) => {
   const movieCards = movies.map((movie) => {
-    return <FilmCard key={movie.id} movie={movie} />
+    return (
+      <FilmCard
+        key={movie.id}
+        movie={movie}
+        showMovieDetails={showMovieDetails}
+      />
+    );
   });
-  return (
-    <section className='films-container'>
-      {movieCards}
-    </section>
-  );
+  return <section className="films-container">{movieCards}</section>;
 };
-
-
 
 export default FilmsContainer;
 
-
-// function 
+// function
 // event target key === specific movie
 // display the movie details
 // and hide the films container
