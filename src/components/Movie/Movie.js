04a10020de01c5953e4a7d7a1362559db092a6a1
Maria DelSignore <maria.ad27@gmail.com>
2021-08-27 19:11:37 -0600

    Access data properly to set state

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -6,33 +6,44 @@ class Movie extends Component {
   constructor() {
     super()
     this.state = {
-      movie: {}
-      video: ''
+      movie: {},
+      video: {}
     }
-    // console.log(this.state.movie)
+
   }
   
   componentDidMount() {
-    // console.log('butts', this.props.movieID)
-    fetchData(`movies/${this.props.movieID}`).then((data) =>
-      this.setState({ movie: data.movie })
-    );
-
-    fetchData(`movies/${this.props.movieID}/videos`).then((data) =>
-    this.setState({ video: data })
-  );
+    return Promise.all([
+      fetchData(`movies/${this.props.movieID}`), 
+      fetchData(`movies/${this.props.movieID}/videos`)
+    ])
+      .then(data => {this.setState({ movie: data[0].movie, video: data[1].videos[0] })})
   }
+
+
 
 
   render() {
     const { title, poster_path, backdrop_path, release_date, overview, genres, budget, runtime, tagline, average_rating } = this.state.movie
+
+        console.log('movie', this.state.movie)
+        console.log('videos', this.state.video.key)
 
     return (
       <section>
         <h2>{title}</h2>
         <h3>{average_rating}</h3>
         <img src={poster_path} alt={`${title} movie poster`} />
-        <>
+        <div>
+          <iframe
+            title='Embedded YouTube Video'
+            width='650'
+            height='380'
+            src={`https://www.youtube.com/embed/${this.state.video.key}`}
+            frameBorder='0'
+            allowFullScreen
+          />
+        </div>
       </section>
     )
   }  
