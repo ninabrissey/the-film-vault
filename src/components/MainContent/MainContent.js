a547f08882b656e4d6f00cc3bdf0d460f006a5e6
Maria DelSignore <maria.ad27@gmail.com>
2021-08-29 19:10:40 -0600

    Add error msg condition to fetched data

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -8,18 +8,23 @@ class MainContent extends Component {
     super(props);
     this.state = {
       movies: [],
+      error: ''
     };
   }
 
   componentDidMount() {
     fetchData('movies').then((data) =>
       this.setState({ movies: [...this.state.movies, ...data.movies] })
-    );
+    )
+      .catch(err => this.setState({error: err}));
   }
+
 
   render() {
     return (
       <div>
+        {this.state.error && <p>{this.state.error}</p>}
+        {!this.state.movies.length && <p>This is where we'll put our loading page</p>}
         {this.props.showMovieDetails && 
           <FilmsContainer
             movies={this.state.movies}
