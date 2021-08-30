4fd4f2752f89f710090accddfd22d9b10e6d4611
Maria DelSignore <maria.ad27@gmail.com>
2021-08-29 18:58:39 -0600

    Check if promise has not resolved

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,6 +1,13 @@
 const fetchData = (type) => {
-  return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/${type}`).then(
-    (res) => res.json()
+  return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/${type}`)
+    .then(res => {
+      if (!res.ok) {
+        throw new Error("Trouble loading. Refresh the page and get your movie on.");
+      } else {
+        return res.json();
+      }
+    }
+  
   );
 };
 
