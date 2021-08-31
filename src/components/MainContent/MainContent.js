d04013c1db6dc26474117600182a1d3e0cca8f9f
Nina Brissey <nbrissey@gmail.com>
2021-08-31 07:36:08 -0600

    Pass formatRating to FilmsContainer

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -8,32 +8,35 @@ class MainContent extends Component {
     super(props);
     this.state = {
       movies: [],
-      error: ''
+      error: '',
     };
   }
 
   componentDidMount() {
-    fetchData('movies').then((data) =>
-      this.setState({ movies: [...this.state.movies, ...data.movies] })
-    )
-      .catch(err => this.setState({error: err}));
+    fetchData('movies')
+      .then((data) =>
+        this.setState({ movies: [...this.state.movies, ...data.movies] })
+      )
+      .catch((err) => this.setState({ error: err }));
   }
-
 
   render() {
     return (
       <div>
         {this.state.error && <p>{this.state.error}</p>}
-        {!this.state.movies.length && <p>This is where we'll put our loading page</p>}
-        {this.props.showMovieDetails && 
+        {!this.state.movies.length && (
+          <p>This is where we'll put our loading page</p>
+        )}
+        {this.props.showMovieDetails && (
           <FilmsContainer
             movies={this.state.movies}
             showMovieDetails={this.props.showMovieDetails}
-          />}
+            formatRating={this.props.formatRating}
+          />
+        )}
       </div>
     );
   }
 }
 
 export default MainContent;
-
