2029e0ee925fd0a9bace8b0c84b35a0a0d9230df
Maria DelSignore <maria.ad27@gmail.com>
2021-08-31 17:11:24 -0600

    Add Link to url path

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,16 +1,19 @@
 import './Nav.css';
 import logo from '../../logo.png';
+import { Link } from 'react-router-dom';
 
 const Nav = (props) => {
   return (
     <header>
       <h1>
-        <img 
+        <Link to='/'>
+          <img 
           className='logo' 
           src={logo} 
           alt='the film vault logo and link to go to main page'
           onClick={() => props.showAllMovies()}
         />
+        </Link>
       </h1>
     </header>
   );
