4b0d35573b57bbc7afbc03411c9ee6cc74ac3536
Nina Brissey <nbrissey@gmail.com>
2021-08-31 07:35:04 -0600

    Pass formatRating to FilmCard

--- src/components/FilmsContainer/FilmsContainer.js
+++ src/components/FilmsContainer/FilmsContainer.js
@@ -1,13 +1,14 @@
 import FilmCard from '../FilmCard/FilmCard';
 import './FilmsContainer.css';
 
-const FilmsContainer = ({ movies, showMovieDetails }) => {
+const FilmsContainer = ({ movies, showMovieDetails, formatRating }) => {
   const movieCards = movies.map((movie) => {
     return (
       <FilmCard
         key={movie.id}
         movie={movie}
         showMovieDetails={showMovieDetails}
+        formatRating={formatRating}
       />
     );
   });
