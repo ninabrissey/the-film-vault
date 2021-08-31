9c27cf3697636b3f2904cd2b8700c2351a5e44cd
Nina Brissey <nbrissey@gmail.com>
2021-08-31 07:37:11 -0600

    Format movie details

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import fetchData from '../../apiCalls';
 import './Movie.css';
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  maximumFractionDigits: 0,
+});
 
 class Movie extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       movie: {},
       video: {},
@@ -20,7 +25,20 @@ class Movie extends Component {
     });
   }
 
+  formatRuntime = (runtime) => {
+    if (runtime < 59) {
+      return `${runtime}min`;
+    }
+    if (runtime > 59 && runtime < 119) {
+      return `1hr ${runtime - 60}min`;
+    }
+    if (runtime > 119) {
+      return `2hr ${runtime - 120}min`;
+    }
+  };
+
   render() {
+    console.log(this.props.formatRating, 'props');
     const {
       title,
       poster_path,
@@ -31,17 +49,27 @@ class Movie extends Component {
       budget,
       runtime,
       tagline,
+      revenue,
       average_rating,
     } = this.state.movie;
+
+    console.log(typeof budget, 'budget');
 
     return (
       <section className="movie-details">
         <div className="details1-container">
           <h2>{title}</h2>
-          <h3>{average_rating}</h3>
+          <h3>
+            {typeof runtime === 'number' &&
+              this.props.formatRating(average_rating)}
+          </h3>
         </div>
         <div className="details2-container">
-          <img className="movie-poster" src={poster_path} alt={`${title} movie poster`} />
+          <img
+            className="movie-poster"
+            src={poster_path}
+            alt={`${title} movie poster`}
+          />
           <iframe
             title="Embedded YouTube Video"
             width="650"
@@ -54,12 +82,13 @@ class Movie extends Component {
         <div className="details3-container">
           <p>Tagline: {tagline}</p>
           <p>Overview: {overview}</p>
-          <p>Realease Date:{release_date}</p>
+          <p>Release Date:{release_date}</p>
           {/* {genres.length > 1 && genres.map((genre) => <p>{genre}</p>)}
           {genres.length === 1 && <p>{genres[0]}</p>} */}
-          <p>Genres: {genres}</p>
-          <p>Runtime: {runtime}</p>
-          <p>Budget: {budget}</p>
+          {genres && <p>Genres: {genres}</p>}
+          {runtime !== 0 && <p>Runtime: {this.formatRuntime(runtime)}</p>}
+          {budget !== 0 && <p>Budget: {budget}</p>}
+          {revenue !== 0 && <p>Revenue: {formatter.format(revenue)}</p>}
         </div>
       </section>
     );
