6ceba52f5c6d742bf9ccfb3fa130f7001be3635e
Maria DelSignore <maria.ad27@gmail.com>
2021-09-01 17:07:43 -0600

    Remove format rating prop

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -29,7 +29,6 @@ class MainContent extends Component {
         )}
         <FilmsContainer
           movies={this.state.movies}
-          formatRating={this.props.formatRating}
         />
       </div>
     );
