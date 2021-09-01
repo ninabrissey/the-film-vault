7967ba6a630e2b1dc71a92dc3fb37aa56f78ef8a
Maria DelSignore <maria.ad27@gmail.com>
2021-09-01 17:06:33 -0600

    Change to functional component and remove methods

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,54 +6,28 @@ import Movie from '../Movie/Movie';
 import logo from '../../logo.png';
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isAllMoviesDisplayed: true,
-      movieID: null,
-    };
-  }
 
-  showMovieDetails = (id) => {
-    this.setState({ isAllMoviesDisplayed: true, movieID: id });
-  };
-
-  showAllMovies = () => {
-    this.setState({ isAllMoviesDisplayed: true });
-  };
-
-  formatRating = (rating) => {
-    let formattedRating = rating.toFixed(1);
-    return formattedRating;
-  };
-
-  render() {
-    return (
-      <div className="app">
-        <Nav />
-        <Route exact path="/" render={() => ( <MainContent
-            movieID={this.state.movieID}
-            formatRating={this.formatRating}
-          />  
-        )} />
-        <Route exact path="/movies/:movieID" render={({ match }) => ( <Movie
-            movieID={match.params.movieID}
-            formatRating={this.formatRating}
-          />
-        )} />
-        <footer>
-          <Link to='/'>
-            <img 
-            className='logo' 
-            src={logo} 
-            alt='the film vault logo and link to go to main page'
-          />
-          </Link>
-        </footer>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="app">
+      <Nav />
+      <Route exact path="/" render={() => ( <MainContent/> )} />
+      <Route exact path="/movies/:movieID" render={({ match }) => ( <Movie
+          movieID={match.params.movieID}
+        />
+      )} />
+      <footer>
+        <Link to='/'>
+          <img 
+          className='logo' 
+          src={logo} 
+          alt='the film vault logo and link to go to main page'
+        />
+        </Link>
+      </footer>
+    </div>
+  );
 }
+
 
 export default App;
