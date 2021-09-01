511f62d267d07463565a8c52d11461b0f6af1691
Maria DelSignore <maria.ad27@gmail.com>
2021-09-01 16:57:29 -0600

    Remove onclick from image

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,6 +1,6 @@
 import './Nav.css';
 import logo from '../../logo.png';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Nav = (props) => {
   return (
@@ -11,10 +11,12 @@ const Nav = (props) => {
           className='logo' 
           src={logo} 
           alt='the film vault logo and link to go to main page'
-          onClick={() => props.showAllMovies()}
         />
         </Link>
       </h1>
+      <NavLink to='/'>
+        <button>All Movies</button>
+      </NavLink>  
     </header>
   );
 };
