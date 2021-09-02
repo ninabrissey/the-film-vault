981bd68ca5f7e2d0038c72d4d643cd3dbc67c5ae
Nina Brissey <nbrissey@gmail.com>
2021-09-01 18:57:35 -0600

    Add test for h3 title

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -2,8 +2,25 @@ beforeEach(() => {
   cy.visit('http://localhost:3000/');
 });
 
-describe('The Film Vault App', () => {
-  it('should be able to visit page', () => {
+describe('The Film Vault Main Page', () => {
+  it('should have an All Movies button', () => {
     cy.get('button').contains('All Movies');
   });
+
+  it('should render the correct elements', () => {
+    cy.get('img')
+      .contains(
+        'https://image.tmdb.org/t/p/original//k68nPLbIST6NP96JmTxmZijEvCA.jpg'
+      )
+      .get('h3')
+      .contains('Tenet');
+  });
+
+  it('should be able to click a movie poster', () => {});
+
+  it('should ', () => {});
+
+  it('should ', () => {});
+
+  it('should ', () => {});
 });
