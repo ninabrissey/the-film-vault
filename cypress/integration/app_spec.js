85d39990616a52e93c52a7754413d651bf71e507
Nina Brissey <nbrissey@gmail.com>
2021-09-02 19:29:45 -0600

    Test that h4 tag contains correct movie rating

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -9,12 +9,16 @@ describe('The Film Vault Main Page', () => {
   });
 
   it('should render the correct elements', () => {
-    cy.get('h3').contains('Money Plane').get('p').contains('2020');
+    cy.get('h3')
+      .contains('Money Plane')
+      .get('p')
+      .contains('2020')
+      .get('h4')
+      .contains('6.1');
   });
 
   it('should be able to click a movie poster', () => {
     cy.get('article>a').eq(0).click();
-    // .contains('http://localhost:3000/movies/694919');
   });
 
   it('should ', () => {});
