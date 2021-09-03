3bdc91884e0b229ba7902d58d50619a35beb7b57
Nina Brissey <nbrissey@gmail.com>
2021-09-02 19:03:58 -0600

    Remove h1 from logo

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,18 +5,16 @@ import { Link, NavLink } from 'react-router-dom';
 const Nav = () => {
   return (
     <header>
-      <h1>
-        <Link to='/'>
-          <img 
-          className='logo' 
-          src={logo} 
-          alt='the film vault logo and link to go to main page'
+      <Link to="/">
+        <img
+          className="logo"
+          src={logo}
+          alt="the film vault logo and link to go to main page"
         />
-        </Link>
-      </h1>
-      <NavLink to='/'>
-        <button className='all-movies-btn'>All Movies</button>
-      </NavLink>  
+      </Link>
+      <NavLink to="/">
+        <button className="all-movies-btn">All Movies</button>
+      </NavLink>
     </header>
   );
 };
