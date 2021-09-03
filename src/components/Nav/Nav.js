a0b8cf3e1e0a3097d86f44262218fccc555522a2
Nina Brissey <nbrissey@gmail.com>
2021-09-02 21:23:09 -0600

    Add SearchBar component

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,6 +1,7 @@
-import './Nav.css';
-import logo from '../../logo.png';
 import { Link, NavLink } from 'react-router-dom';
+import SearchBar from '../SearchBar/SearchBar';
+import logo from '../../logo.png';
+import './Nav.css';
 
 const Nav = () => {
   return (
@@ -12,6 +13,7 @@ const Nav = () => {
           alt="the film vault logo and link to go to main page"
         />
       </Link>
+      <SearchBar />
       <NavLink to="/">
         <button className="all-movies-btn">All Movies</button>
       </NavLink>
