08755a609bfdfa2829d4dd6307a6ae0c2bb02719
Nina Brissey <nbrissey@gmail.com>
2021-09-02 21:28:25 -0600

    Add input element and state

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,8 +2,18 @@ import { Component } from 'react';
 import './SearchBar.css';
 
 class SearchBar extends Component {
+  constructor() {
+    super()
+    this.state = {
+      searchInput = ''
+    }
+  }
   render() {
-    return <form></form>;
+    return (
+      <form>
+        <input aria-label="search bar" placeholder="Search" name="searchInput" value={this.state.searchInput}/>
+      </form>
+    );
   }
 }
 
