2a369426058fb31026ee2a68b036bcc40cfbbb79
Nina Brissey <nbrissey@gmail.com>
2021-09-02 21:33:53 -0600

    Add handleChange method for search

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -3,15 +3,26 @@ import './SearchBar.css';
 
 class SearchBar extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      searchInput = ''
-    }
+      searchInput: '',
+    };
   }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   render() {
     return (
       <form>
-        <input aria-label="search bar" placeholder="Search" name="searchInput" value={this.state.searchInput}/>
+        <input
+          aria-label="search bar"
+          placeholder="Search"
+          name="searchInput"
+          value={this.state.searchInput}
+          onChange={(e) => this.handleChange(e)}
+        />
       </form>
     );
   }
