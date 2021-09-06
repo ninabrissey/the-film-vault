6fd05fb9ad98bd5dd9b1a3f6e977aae0cd4c6ee8
Maria DelSignore <maria.ad27@gmail.com>
2021-09-06 17:51:31 -0600

    Relocated Nav component into MainContent

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,30 +1,33 @@
-import { Route, Link } from 'react-router-dom';
-import Nav from '../Nav/Nav';
+import { Route, Link, Switch } from 'react-router-dom';
+// import Nav from '../Nav/Nav';
 import MainContent from '../MainContent/MainContent';
 import Movie from '../Movie/Movie';
-import logo from '../../logo.png';
+// import logo from '../../logo.png';
 import './App.css';
 
 const App = () => {
   return (
     <div className="app">
-      <Nav />
-      <Route exact path="/" render={() => <MainContent />} />
-      <Route exact path="/search" render={() => <MainContent />} />
-      <Route
-        exact
-        path="/movies/:movieID"
-        render={({ match }) => <Movie movieID={match.params.movieID} />}
-      />
-      <footer>
-        <Link to="/">
+      {/* <Nav /> */}
+      <Switch>
+        <Route exact path="/" render={() => <MainContent />} />
+        <Route exact path="/search" render={() => <MainContent />} />
+        <Route
+          exact
+          path="/movies/:movieID"
+          render={({ match }) => <Movie movieID={match.params.movieID} />}
+        />
+        <Route render={() => <p>page not found</p>}/>
+      </Switch>  
+      {/* <footer>
+        <Link to="/" onClick={() => {window.location.reload()}}>
           <img
             className="logo"
             src={logo}
             alt="the film vault logo and link to go to main page"
           />
         </Link>
-      </footer>
+      </footer> */}
     </div>
   );
 };
