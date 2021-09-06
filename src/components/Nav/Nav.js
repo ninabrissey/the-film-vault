41e4944b200a9e87ea686364c1f89359973be786
Maria DelSignore <maria.ad27@gmail.com>
2021-09-06 17:53:26 -0600

    Update all movies btn to a home btn

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,24 +3,29 @@ import logo from '../../logo.png';
 import partyTitle from '../../party-title.png'
 import './Nav.css';
 
-const Nav = () => {
+const Nav = ({movieObj}) => {
+
   return (
     <header>
-      <div className='image-overlay'></div>
-      <img className='header-image' src='https://image.tmdb.org/t/p/original//54yOImQgj8i85u9hxxnaIQBRUuo.jpg' alt='ava backdrop' />
-      <div className='nav-navigation'>
-        <Link to="/">
-          <img
-            className="logo"
-            src={logo}
-            alt="the film vault logo and link to go to main page"
-          />
-        </Link>
-        <NavLink to="/">
-          <button className="all-movies-btn">All Movies</button>
-        </NavLink>
-      </div>
-      <img className='party-title' src={partyTitle} alt='party title' />
+      <div className='image-overlay'/>
+      {movieObj && <img className='header-image' src={movieObj.backdrop_path} />}
+        <div className='nav-navigation'>
+          <Link to="/">
+            <img
+              // onClick={() => {window.location.reload()}}
+              className="logo"
+              src={logo}
+              alt="the film vault logo and link to go to main page"
+            />
+          </Link>
+          <NavLink to="/">
+            <button 
+            // onClick={() => {window.location.reload()}}
+            className="all-movies-btn">Home</button>
+          </NavLink>
+        </div>
+        <img className='party-title' src={partyTitle} alt='the good, the bad and the mediocre header' />
+      
     </header>
   );
 };
