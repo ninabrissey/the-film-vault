6d58299e1d4edf6d9368de50afb45e2d7ed423db
Maria DelSignore <maria.ad27@gmail.com>
2021-09-06 12:53:42 -0600

    Update props in handle change method

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -12,28 +12,28 @@ class SearchBar extends Component {
 
   handleChange = (e) => {
     this.setState({ [e.target.name]: e.target.value });
-    this.props.filterMovies(this.state.searchInput.toLowerCase());
+    this.props.filterMovies(e.target.value.toLowerCase());
   };
 
   render() {
     return (
       <form>
-        <Link to="/search">
+        {/* <Link to="/search"> */}
           <input
             className="search-bar"
             aria-label="search bar"
             placeholder="Search"
             name="searchInput"
             value={this.state.searchInput}
-            onChange={(e) => this.handleChange(e)}
+            onChange={this.handleChange}
             autoComplete="off"
           />
-        </Link>
-        <Link to="/">
-          <button onClick={() => this.props.clearFilteredMovies()}>
+        {/* </Link> */}
+        {/* <Link to="/"> */}
+          <button onClick={this.props.clearFilteredMovies}>
             clear
           </button>
-        </Link>
+        {/* </Link> */}
       </form>
     );
   }
