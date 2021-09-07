4d61cfe64a67d5037474c1cf4c02f658b3109e44
Maria DelSignore <maria.ad27@gmail.com>
2021-09-06 18:01:07 -0600

    Remove unused code

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,11 @@
-import { Route, Link, Switch } from 'react-router-dom';
-// import Nav from '../Nav/Nav';
+import { Route, Switch } from 'react-router-dom';
 import MainContent from '../MainContent/MainContent';
 import Movie from '../Movie/Movie';
-// import logo from '../../logo.png';
 import './App.css';
 
 const App = () => {
   return (
     <div className="app">
-      {/* <Nav /> */}
       <Switch>
         <Route exact path="/" render={() => <MainContent />} />
         <Route exact path="/search" render={() => <MainContent />} />
@@ -19,15 +16,7 @@ const App = () => {
         />
         <Route render={() => <p>page not found</p>}/>
       </Switch>  
-      {/* <footer>
-        <Link to="/" onClick={() => {window.location.reload()}}>
-          <img
-            className="logo"
-            src={logo}
-            alt="the film vault logo and link to go to main page"
-          />
-        </Link>
-      </footer> */}
+      {/* need to make 404 page component */}
     </div>
   );
 };
