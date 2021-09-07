e5d5cceb7ebefa2679f8cc1bf7a67e0036eb094a
Maria DelSignore <maria.ad27@gmail.com>
2021-09-07 02:50:49 -0600

    Add conditional render to header for movie page

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,34 +3,32 @@ import logo from '../../logo.png';
 import partyTitle from '../../party-title.png'
 import './Nav.css';
 
-const Nav = ({movieObj, isMovieDisplayed}) => {
+const Nav = ({movieObj}) => {
 
   return (
-    <header className='all-movies-header'>
+    <>
+    {movieObj && <header className='all-movies-header'>
+      <div className='image-overlay'/>
+      <img className='header-image' src={movieObj.backdrop_path} alt={`${movieObj.title} backdrop`} />
+      <img className='party-title' src={partyTitle} alt='the good, the bad and the mediocre header' />
+    </header>}
 
-    {isMovieDisplayed && 
-      <header className='movie-view-header' />}
-
-      {movieObj && <div className='image-overlay'/>}
-      {movieObj && <img className='header-image' src={movieObj.backdrop_path} alt={`${movieObj.title} backdrop`} />}
-      {movieObj && <img className='party-title' src={partyTitle} alt='the good, the bad and the mediocre header' />}
-
-        <div className='nav-navigation'>
-          <Link to="/">
-            <img
-              // onClick={() => {window.location.reload()}}
-              className="logo"
-              src={logo}
-              alt="the film vault logo and link to go to main page"
-            />
-          </Link>
-          <NavLink to="/">
-            <button 
+      <div className='nav-navigation'>
+        <Link to="/">
+          <img
             // onClick={() => {window.location.reload()}}
-            className="all-movies-btn">Home</button>
-          </NavLink>
-        </div>
-    </header>
+            className="logo"
+            src={logo}
+            alt="the film vault logo and link to go to main page"
+          />
+        </Link>
+        <NavLink to="/">
+          <button 
+          // onClick={() => {window.location.reload()}}
+          className="all-movies-btn">Home</button>
+        </NavLink>
+      </div>
+    </>
   );
 };
 
