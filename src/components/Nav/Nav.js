2c34d72da1d83ecb87f402ff5ade53787717ddba
Maria DelSignore <maria.ad27@gmail.com>
2021-09-07 03:13:16 -0600

    Add conditional for home button to clear input

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,14 +4,13 @@ import partyTitle from '../../party-title.png'
 import './Nav.css';
 
 const Nav = ({movieObj}) => {
-
   return (
     <>
-    {movieObj && <header className='all-movies-header'>
-      <div className='image-overlay'/>
-      <img className='header-image' src={movieObj.backdrop_path} alt={`${movieObj.title} backdrop`} />
-      <img className='party-title' src={partyTitle} alt='the good, the bad and the mediocre header' />
-    </header>}
+      {movieObj && <header className='all-movies-header'>
+        <div className='image-overlay'/>
+        <img className='header-image' src={movieObj.backdrop_path} alt={`${movieObj.title} backdrop`} />
+        <img className='party-title' src={partyTitle} alt='the good, the bad and the mediocre header' />
+      </header>}
 
       <div className='nav-navigation'>
         <Link to="/">
@@ -22,11 +21,11 @@ const Nav = ({movieObj}) => {
             alt="the film vault logo and link to go to main page"
           />
         </Link>
-        <NavLink to="/">
+        {movieObj && <button onClick={() => {window.location.reload()}} className="all-movies-btn">Home</button>}
+        {!movieObj && <NavLink to="/">
           <button 
-          // onClick={() => {window.location.reload()}}
-          className="all-movies-btn">Home</button>
-        </NavLink>
+            className="all-movies-btn">Home</button>
+        </NavLink>}
       </div>
     </>
   );
