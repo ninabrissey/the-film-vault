03dcc3f865f6baa974dee23b9201749c929ab2b7
Maria DelSignore <maria.ad27@gmail.com>
2021-09-06 22:11:55 -0600

    Add logo to 404 page

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,14 @@
-import './NotFound.css'
-
+import './NotFound.css';
+import loadingLogo from '../../loadingLogo.png';
 const NotFound = () => {
   return (
     <div className='not-found'>
-      <p className='not-found-text'>Oops...</p>
-      <p className='not-found-text'>Seems we cannot find that page</p>
+      <div className='not-found-text'>
+        <p>Oops...</p>
+        <p>Seems we cannot find the page you requested</p>
+      </div>
+      <p className='not-found-stat'>404</p>
+      <img className='not-found-logo' src={loadingLogo} alt='the film vault logo' />
     </div>
   )
 }
