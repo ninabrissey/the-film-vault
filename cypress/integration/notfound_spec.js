3e66229e08a14c735d71af8d5be82b5f3ecb9cea
Nina Brissey <nbrissey@gmail.com>
2021-09-07 01:40:04 -0600

    Add test for p tag at position 0 and pass

--- cypress/integration/notfound_spec.js
+++ cypress/integration/notfound_spec.js
@@ -1,3 +1,7 @@
 describe('The Film Vault Not Found 404 Error Page', () => {
-  cy.visit('http://localhost:3000/film');
+  it('should see a 404 page if the path is incorrect', () => {
+    cy.visit('http://localhost:3000/film');
+
+    cy.get('p').eq(0).contains('Oops...');
+  });
 });
