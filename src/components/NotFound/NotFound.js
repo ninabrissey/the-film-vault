18f186a02789328f1d3ac493a226da7d4da9d54e
Maria DelSignore <maria.ad27@gmail.com>
2021-09-06 21:19:00 -0600

    Create NotFound component

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import './NotFound.css'
+
+const NotFound = () => {
+  return (
+    <div className='not-found'>
+      <p>404:</p>
+      <p>Page Not Found</p>
+    </div>
+  )
+}
+
+export default NotFound;
