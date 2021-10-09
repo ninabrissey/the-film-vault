61b4ba87caf17436ae2c4fa83b41db22c509ae09
Nina Brissey <nbrissey@gmail.com>
2021-10-09 14:12:46 -0700

    Correct spelling error

--- cypress/integration/search_spec.js
+++ cypress/integration/search_spec.js
@@ -3,7 +3,7 @@ beforeEach(() => {
   cy.fetchAllMovies('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2');
 });
 
-describe('The Film Vault Seeach Feature', () => {
+describe('The Film Vault Search Feature', () => {
   it('should be able to search movies by title', () => {
     cy.get('input[name="searchInput"]')
       .type('Av')
